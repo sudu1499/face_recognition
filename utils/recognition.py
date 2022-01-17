3c7cd22c791bbdbf38a8b53226dd94a81a7fd8cb
sudarshan n <suduanagha@gmail.com>
2022-01-17 14:13:52 +0530

    added recognizer

--- utils/recognition.py
+++ utils/recognition.py
@@ -2,10 +2,14 @@ import tensorflow as tf
 from  tensorflow.keras.models import load_model
 import cv2
 import get_face
+import numpy as np
 
-
-#vid=cv2.VideoCapture(0)
-#while 1:
- #   _,frame=vid.read()
+vid=cv2.VideoCapture(0)
+while 1:
+    _,frame=vid.read()
+    frame=get_face(frame)
+    model=load_model()
+    p=model.predict(frame)
+    print(np.argmax(p))
 
     
